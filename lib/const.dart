c4674d3be84508d473d77b9e12f352c152cd401a
Adesanya David <tito@mytransita.com>
2022-06-27 14:59:54 +0100

    Updates on Login and Sign up

---
+++ lib/const.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/material.dart';
+
+var backgroundColor = Colors.black;
+
+var buttonColor = Colors.blue[300];
---
+++ lib/views/screens/auth/login_Screen.dart
@@ -0,0 +1,84 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_app/const.dart';
+import 'package:flutter_app/views/screens/auth/signUp_Screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Welcome, Please Enter your details to unlock Maximum potentials", style: TextStyle(fontWeight: FontWeight.bold),),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                hintText: 'Enter Email Address',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none
+                ),
+                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black))
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  filled: true,
+                  hintText: 'Enter Password',
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide.none
+                  ),
+                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black))
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width -40,
+              height: 50,
+              decoration: BoxDecoration(
+                color: buttonColor,
+              ),
+              child: Center(
+                  child: InkWell(
+                      onTap: (){
+                        print('Login Successful');
+                      },
+                      child: Text('Login', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),))
+                ,)
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Need an account?', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,),),
+                SizedBox(
+                  width: 6,
+                ),
+                InkWell(
+                    onTap: (){
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Signup()));
+                    },
+                    child: Text('Sign Up!', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,),))
+              ],
+            )
+
+          ],
+        ),
+      )
+    );
+  }
+}
