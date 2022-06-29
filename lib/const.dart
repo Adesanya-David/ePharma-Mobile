bd742543dfadfbce1c4600dacc62640e9ff78296
Adesanya David <tito@mytransita.com>
2022-06-29 17:24:57 +0100

    Updates

--- lib/const.dart
+++ lib/const.dart
@@ -4,3 +4,5 @@ import 'package:flutter/material.dart';
 var backgroundColor = Colors.black;
 
 var buttonColor = Colors.blue[300];
+
+var textButtonColor = Colors.white;
--- lib/views/screens/auth/signUp_Screen.dart
+++ lib/views/screens/auth/signUp_Screen.dart
@@ -10,95 +10,116 @@ class Signup extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        alignment: Alignment.center,
         padding: EdgeInsets.all(20),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Stack(
             children: [
-        TextField(
+              CircleAvatar(
+                radius: 64,
+                backgroundColor: Colors.grey,
+              ),
+              Positioned(
+                right: 5,
+                bottom: 1,
+                child: Icon(Icons.add_a_photo),
+
+              )
+            ],
+          ),
+          // Text("Welcome, Please Enter your details to unlock Maximum potentials", style: TextStyle(fontWeight: FontWeight.bold),),
+          SizedBox(
+            height: 40,
+          ),
+          TextField(
+            decoration: InputDecoration(
+                filled: true,
+                hintText: 'Enter Full Name',
+                border: OutlineInputBorder(
+                    borderSide: BorderSide.none
+                ),
+                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black))
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          TextField(
           decoration: InputDecoration(
-              filled: true,
-              hintText: 'Enter Full Name',
+          filled: true,
+              hintText: 'Enter Username',
               border: OutlineInputBorder(
                   borderSide: BorderSide.none
               ),
               focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black))
           ),
-        ),
-        SizedBox(
-          height: 15,
-        ),
-        TextField(
-        decoration: InputDecoration(
-        filled: true,
-            hintText: 'Enter Username',
-            border: OutlineInputBorder(
-                borderSide: BorderSide.none
-            ),
-            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black))
-        ),
-      ),
-      SizedBox(
-        height: 15,
-      ),
-      TextField(
-        decoration: InputDecoration(
-            filled: true,
-            hintText: 'Enter Email Address',
-            border: OutlineInputBorder(
-                borderSide: BorderSide.none
-            ),
-            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black))
-        ),
-      ),
-      SizedBox(
-        height: 15,
-      ),
-      TextField(
-        decoration: InputDecoration(
-            filled: true,
-            hintText: 'Enter Password',
-            border: OutlineInputBorder(
-                borderSide: BorderSide.none
-            ),
-            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black))
-        ),
-      ),
-      SizedBox(
-        height: 15,
-      ),
-      Container(
-          width: MediaQuery.of(context).size.width -40,
-          height: 50,
-          decoration: BoxDecoration(
-            color: buttonColor,
           ),
-          child: Center(
-            child: InkWell(
-                onTap: (){
-                  print('Account Created Successfully');
-                },
-                child: Text('Sign Up', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),))
-            ,)
-      ),
-      SizedBox(
-        height: 10,
-      ),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Already have an account?', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,),),
           SizedBox(
-            width: 6,
+            height: 15,
           ),
-          InkWell(
-              onTap: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>LoginScreen()));
-              },
-              child: Text('Log In!', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,),))
-        ],
-      )
+          TextField(
+            decoration: InputDecoration(
+                filled: true,
+                hintText: 'Enter Email Address',
+                border: OutlineInputBorder(
+                    borderSide: BorderSide.none
+                ),
+                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black))
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          TextField(
+            decoration: InputDecoration(
+                filled: true,
+                hintText: 'Enter Password',
+                border: OutlineInputBorder(
+                    borderSide: BorderSide.none
+                ),
+                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black))
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width -40,
+            height: 50,
+            decoration: BoxDecoration(
+              color: buttonColor,
+            ),
+            child: Center(
+              child: InkWell(
+                  onTap: (){
+                    print('Account Created Successfully');
+                  },
+                  child: Text('Sign Up', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),))
+            ,)
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Already have an account?', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,),),
+              SizedBox(
+                width: 6,
+              ),
+              InkWell(
+                  onTap: (){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>LoginScreen()));
+                  },
+                  child: Text('Log In!', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,),))
+            ],
+          )
 
     ]
-    ),));
+    ),
+        ),));
   }
 }
