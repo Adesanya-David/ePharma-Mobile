386a45dbc5be0563d11707631ab57095a5fb6ec6
Adesanya David <tito@mytransita.com>
2022-07-09 00:29:24 +0100

    Updates on adding Images to profile

--- lib/const.dart
+++ lib/const.dart
@@ -1,6 +1,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 var backgroundColor = Colors.black;
@@ -13,3 +14,5 @@ var textButtonColor = Colors.white;
 var firebaseAuth = FirebaseAuth.instance;
 
 var firebaseStore = FirebaseFirestore.instance;
+
+var firebaseStorage = FirebaseStorage.instance;
